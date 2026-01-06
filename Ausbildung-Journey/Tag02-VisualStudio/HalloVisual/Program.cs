6276dc53f582e6e8f2ec5bf55036ee701b766256
techsly(sylvester) <slythesly0@gmail.com>
2026-01-06 23:20:49 +0100

    Update Program.cs

--- Ausbildung-Journey/Tag02-VisualStudio/HalloVisual/Program.cs
+++ Ausbildung-Journey/Tag02-VisualStudio/HalloVisual/Program.cs
@@ -1,12 +1,1 @@
-﻿namespace Kap2
-{
-    using System;
 
-    public class Hallo
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hallo .NET");
-        }
-    }
-}
