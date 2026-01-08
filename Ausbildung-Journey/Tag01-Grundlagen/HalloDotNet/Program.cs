685ba34149287b69b8313df3d0f21d523733ce54
techsly(sylvester) <slythesly0@gmail.com>
2026-01-08 22:29:16 +0100

    Add initial .NET program to display 'Hallo .NET'

--- Ausbildung-Journey/Tag01-Grundlagen/HalloDotNet/Program.cs
+++ Ausbildung-Journey/Tag01-Grundlagen/HalloDotNet/Program.cs
@@ -1,1 +1,13 @@
+﻿// Erstes Programm (Clean State for Tag 2)
+namespace Kap1
+{
+    using System;
 
+    public class CHalloEins
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo .NET");
+        }
+    }
+}
