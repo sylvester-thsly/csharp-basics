e7d59ec9897d8f525a44b7a64ae129f1b4e137d8
Sylvester <sylvestertech.sly@gmail.com>
2026-01-18 05:34:32 +0100

    feat: Day 1 - First C# program with variables

---
+++ Projects/Helloword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Sylvester");
+        Console.WriteLine("I am learning C#!");
+        
+        // Variables
+        int age = 25;
+        string name = "Sylvester";
+        double height = 1.75;
+        bool isStudent = true;
+        char grade = 'A';
+        
+        // Print them
+        Console.WriteLine($"Myageis " +  age);
+
+        Console.WriteLine ($"name is " + name);
+
+        Console.WriteLine ($"Myhightis " + height);
+
+        Console.WriteLine ($"am a student " + isStudent);
+
+        Console.WriteLine ($"grade " + grade);
+        
+    
+    }
+} 
